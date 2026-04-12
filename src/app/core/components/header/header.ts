cb40655082e5254e5835c906ef6184aa1daf0415
grGrimales <grojasgrimales@gmail.com>
2026-04-12 09:00:38 -0300

    refactor: remove OnDestroy and Subscription from HeaderComponent

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,9 +1,10 @@
-import { Component, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core'; // 1. Quité OnDestroy de aquí
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { Subject, debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
+import { Subject, debounceTime, distinctUntilChanged } from 'rxjs'; // 2. Quité Subscription de aquí
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-header',
@@ -12,26 +13,20 @@ import { Subject, debounceTime, distinctUntilChanged, Subscription } from 'rxjs'
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class HeaderComponent implements OnDestroy {
+export class HeaderComponent {
   @Output() search = new EventEmitter<string>();
 
   private searchSubject = new Subject<string>();
-  private searchSubscription: Subscription;
 
   constructor() {
-    this.searchSubscription = this.searchSubject.pipe(
+    this.searchSubject.pipe(
       debounceTime(300),
-      distinctUntilChanged()
-    ).subscribe(value => {
-      this.search.emit(value);
-    });
+      distinctUntilChanged(),
+      takeUntilDestroyed()
+    ).subscribe(value => this.search.emit(value));
   }
 
   onSearch(value: string) {
     this.searchSubject.next(value);
-  }
-
-  ngOnDestroy() {
-    this.searchSubscription.unsubscribe();
   }
 }
